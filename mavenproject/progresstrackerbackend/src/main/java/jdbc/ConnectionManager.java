468af9f029da330bd1817cbc99cbf6b54711366c
cobyzim <cobyzimmerman.home@gmail.com>
2022-11-11 12:17:04 -0700

    added .props file and connmanager test file for jdbc

---
+++ mavenproject/progresstrackerbackend/src/main/java/jdbc/ConnectionManager.java
@@ -0,0 +1,72 @@
+package jdbc;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionManager {
+
+	private static Connection connection = null;
+	
+	private static void makeConnection() {
+		
+		// Properties will be used to access our properties file and read its values 
+		Properties props = new Properties();
+		
+		// use the FileInputStream to load in the values from the file to props
+		try {
+			props.load(new FileInputStream("resources/config.properties"));
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		// save the values to the variables, use the same name as what is written in the
+		// file to get the values
+		String url = props.getProperty("url");
+		String username = props.getProperty("username");
+		String password = props.getProperty("password");
+		
+		
+		// finally, establish the connection
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Established connection");
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	// will establish the connection if it it is not already made and return connection
+	// object, making this a singleton design
+	public static Connection getConnection() {
+		
+		if(connection == null) {
+			makeConnection();
+		}
+		
+		return connection;
+	}
+
+public static void main(String[] args) {
+
+	Connection conn = ConnectionManager.getConnection();
+	System.out.println("Made Connection");
+	
+	try {
+		conn.close();
+		System.out.println("Closed connection");
+		
+		
+	} catch(SQLException e) {
+		e.printStackTrace();
+	}
+
+}
+	
+}
